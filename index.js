d456d8c5a13e5be8bd3d3fd670a8dfc3eadaa8db
bartjo666 <bartjo666@gmail.com>
2019-08-28 17:31:27 +0100

    create function to build calculator and formated it

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('hello world');
+
+const CALCBODY = document.querySelector('.calc-numbers');
+const OPERATORS = ['-', '+', '/', '*'];
+
+let a = 0;
+
+const buildCalc = function(){
+    for(i = 1; i < 10; i++){
+        CALCBODY.innerHTML += `<div class="numbers">${i}</div>`
+    }
+    CALCBODY.innerHTML += `<div class="numbers">0</div>`;
+
+}
+
+buildCalc();
+
+console.log(a);
